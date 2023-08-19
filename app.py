0cb3dca0cd426b8bd6f44448f1b28c8012244bcc
farhanpare <119167141+farhanpare@users.noreply.github.com>
2023-08-19 16:08:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get('PORT', 5000))  # Gunakan 5000 sebagai port default
+    app.run(host="0.0.0.0", port=port)
