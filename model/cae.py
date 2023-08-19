01ef297983d60434dc81418cdba345dd528a85f4
farhanpare <119167141+farhanpare@users.noreply.github.com>
2023-08-19 16:02:52 +0700

    Update cae.py

--- model/cae.py
+++ model/cae.py
@@ -113,8 +113,7 @@ class CAE(Model):
         eblock2 = self.e_block_2(eblock1) + eblock1
         eblock3 = self.e_block_3(eblock2) + eblock2
         ec3 = self.e_conv_3(eblock3)
-
-        return self.decode(ec3)
+        return ec3
   
     def decode(self, encoded_value: Tensor) -> Tensor:
         y = encoded_value
@@ -126,3 +125,4 @@ class CAE(Model):
         dec = self.d_conv_3(uc2)
 
         return dec
+    
