e9d95e62276c7dc95b6e5931cffe61e0a513d1b5
farhanpare <119167141+farhanpare@users.noreply.github.com>
2023-08-25 15:17:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,16 @@ def index():
 
     return render_template('index.html', original_image=None, compressed_image=None)
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Gunakan 5000 sebagai port default
